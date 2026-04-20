fd22a5f4967100e7918dc321c5462658c8966863
Thibsurf <146008440+Thibsurf@users.noreply.github.com>
2026-04-21 10:59:16 +1100

    Create spots.js

---
+++ assets/spots.js
@@ -0,0 +1,14 @@
+const SPOT_STATIONS = {
+  'Grand bac': 'dumb',
+  'Petit U': 'dumb',
+  'Gros nem': 'dumb',
+  'Fausse passe Dumbéa': 'dumb',
+  'Gauche de Dumbéa': 'dumb',
+  'Droite de Dumbéa': 'dumb',
+
+  'Ouano': 'ouano',
+  'Roche percée': 'ouano',
+
+  'Skatepark': 'noumea',
+  'Trois cailloux': 'noumea'
+};
